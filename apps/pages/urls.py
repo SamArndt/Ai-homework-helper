786329605b947465db9a05dfa85fdfee8d2f3522
SamArndt <sarndt196@gmail.com>
2026-02-13 15:44:17 -0700

    Added sign up for implementation

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -1,13 +1,15 @@
 from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
 from django.contrib.auth.decorators import login_required
+from .forms import PagesLoginForm
 from django.urls import path
 from pages import views
 
 app_name = "pages"
 
 urlpatterns = [
-    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("signup/", views.SignUpView.as_view(), name="signup"),
+    path("login/", auth_views.LoginView.as_view(template_name="registration/login.html", authentication_form=PagesLoginForm), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("", TemplateView.as_view(template_name="pages/home.html"), name = "home"),
     path("dashboard", login_required(TemplateView.as_view(template_name="pages/dashboard.html")), name="dashboard"),
