81d35658d822959b0ffb3759814b11528c3e6981
SamArndt <sarndt196@gmail.com>
2026-02-13 13:54:27 -0700

    Cleaned up template and added protected dashboard view

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -1,4 +1,6 @@
 from django.contrib.auth import views as auth_views
+from django.views.generic.base import TemplateView
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 from pages import views
 
@@ -7,5 +9,6 @@ app_name = "pages"
 urlpatterns = [
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-    path("", views.home, name="home"),
+    path("", TemplateView.as_view(template_name="pages/home.html"), name = "home"),
+    path("dashboard", login_required(TemplateView.as_view(template_name="pages/dashboard.html")), name="dashboard"),
 ]
