5796bcf900908c89c3c1a30dccfcc34846bc6e56
SamArndt <sarndt196@gmail.com>
2026-02-14 14:38:36 -0700

    Configure dashboard-react for Django api-server and CSRF

--- dashboard-react/src/main.jsx
+++ dashboard-react/src/main.jsx
@@ -1,7 +1,13 @@
+import axios from 'axios'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import './index.css'
+
+// Django's defualt CSRF settings
+axios.defaults.xsrfCookieName = 'csrftoken'
+axios.defaults.xsrfHeaderName = 'X-CSRFToken'
+axios.defaults.withCredentials = true
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
