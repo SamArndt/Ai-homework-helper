bd71dafad7a189ede5109cf00d2e87643031a4fb
Max Roland <maroland@asu.edu>
2026-02-16 22:48:24 -0500

    Replaced the default logger to recreate filtered log behavior from previous api. Adjusted example .env to reflect default value

---
+++ api-server/scripts/utilities/filtered_logs.py
@@ -0,0 +1,50 @@
+import logging
+
+class LogFormatter(logging.Formatter):
+    COLORS = {
+        logging.DEBUG: "\033[94m",
+        logging.INFO: "\033[92m",
+        logging.WARNING: "\033[93m",
+        logging.ERROR: "\033[91m",
+        logging.CRITICAL: "\033[95m",
+    }
+
+    EMOJIS = {
+        logging.DEBUG: "🐞",
+        logging.INFO: "ℹ️",
+        logging.WARNING: "⚠️",
+        logging.ERROR: "❌",
+        logging.CRITICAL: "🔥",
+    }
+
+    RESET = "\033[0m"
+
+    def format(self, record: logging.LogRecord) -> str:
+        color = self.COLORS.get(record.levelno, "")
+        emoji = self.EMOJIS.get(record.levelno, "")
+
+        # Let base formatter build the normal message first
+        formatted_message = super().format(record)
+
+        # Tint the entire line
+        return f"{color}{emoji}  {formatted_message}{self.RESET}"
+    
+
+logger = logging.getLogger(__name__)
+
+def log(message):
+    logger.debug(message)
+
+def log(message, level=logging.INFO):
+    match(level):
+        case logging.DEBUG:
+            logger.debug(message)
+        case logging.INFO:
+            logger.info(message)
+        case logging.WARNING:
+            logger.warning(message)
+        case logging.ERROR:
+            logger.error(message)
+        case logging.CRITICAL:
+            logger.critical(message)
+    
