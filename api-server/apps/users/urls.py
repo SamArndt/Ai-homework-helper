09519a954fb36fbf720980d1d571d44c3a3770d0
scarndt <sarndt196@gmail.com>
2026-02-19 20:56:22 -0700

    Feature/me endpoint (#10)
    * Configured me endpoint for api
    
    * Added test for me endpoint
    
    * Added test for me endpoint
    
    ---------
    
    Co-authored-by: maroland-student <77713290+maroland-student@users.noreply.github.com>

--- api-server/apps/users/urls.py
+++ api-server/apps/users/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
 from users.views import SignupAPIView, CustomObtainAuthToken
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet
 
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('signup/', SignupAPIView.as_view(), name='api_signup'),
-    path('login/', CustomObtainAuthToken.as_view(), name='api_login'), 
+    path('login/', CustomObtainAuthToken.as_view(), name='api_login'),
+    path('', include(router.urls)), 
 ]
--- api-server/apps/users/views.py
+++ api-server/apps/users/views.py
@@ -1,10 +1,12 @@
-from rest_framework import status, generics
+from rest_framework import status, generics, viewsets, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework.authtoken.models import Token
+from rest_framework.decorators import action
+from django.contrib.auth.models import User
 from .models import User
-from .serializers import UserSignupSerializer, CustomAuthTokenSerializer
+from .serializers import UserSignupSerializer, CustomAuthTokenSerializer, UserSerializer
 
 class SignupAPIView(generics.CreateAPIView):
     queryset = User.objects.all()
@@ -19,5 +21,15 @@ class SignupAPIView(generics.CreateAPIView):
         response.data['token'] = token.key
         return response
 
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    @action(detail=False, methods=['get'], permission_classes=[permissions.IsAuthenticated])
+    def me(self, request):
+        user = request.user
+        serializer = self.get_serializer(user)
+        return Response(serializer.data)
+        
 class CustomObtainAuthToken(ObtainAuthToken):
     serializer_class = CustomAuthTokenSerializer
