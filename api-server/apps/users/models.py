e6903ef1b32e9b055cc8e2f1fc1f8122430ebfc1
maroland-student <77713290+maroland-student@users.noreply.github.com>
2026-02-21 22:01:13 -0500

    Added model UserPreferences to store user app preferences. Added post save signal to ensure preferences entry exists for users. Added /preferences/ route to view.py, used to get or post changes to user preferences. Added serializer for new UserPreferences model (#12)

--- api-server/apps/users/models.py
+++ api-server/apps/users/models.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from .managers import CustomUserManager
+from django.conf import settings
 
 
 class User(AbstractUser):
-    """Custom user: email as identifier, profile """
+    # Custom user: email as identifier, profile 
     username = None
     email = models.EmailField(unique=True)
 
@@ -15,3 +16,28 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+class UserPreferences(models.Model):
+    # Model for user preferences
+    # TODO
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='settings',
+        primary_key=True
+    )
+
+    # Should the front end render dark mode?
+    # TODO
+    dark_mode = models.BooleanField(default=True)
+
+    # Should the user receive notifications from the app?
+    # TODO
+    notifications_enabled = models.BooleanField(default=True)
+
+    # Should the user receive email reports from students automatically? Not impacted by notifications preference.
+    # TODO
+    auto_email_reports = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.user.email} Settings"
--- api-server/apps/users/views.py
+++ api-server/apps/users/views.py
@@ -1,3 +1,5 @@
+from urllib import request
+
 from rest_framework import status, generics, viewsets, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
@@ -5,8 +7,8 @@ from rest_framework.permissions import AllowAny
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import action
 from django.contrib.auth.models import User
-from .models import User
-from .serializers import UserSignupSerializer, CustomAuthTokenSerializer, UserSerializer
+from .models import User, UserPreferences
+from .serializers import UserSignupSerializer, CustomAuthTokenSerializer, UserSerializer, UserPreferencesSerializer
 
 class SignupAPIView(generics.CreateAPIView):
     queryset = User.objects.all()
@@ -25,10 +27,31 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    def get_serializer_class(self):
+        if(self.action == 'preferences'):
+            return UserPreferencesSerializer
+        
+        return UserSerializer
+
     @action(detail=False, methods=['get'], permission_classes=[permissions.IsAuthenticated])
     def me(self, request):
         user = request.user
+
         serializer = self.get_serializer(user)
+        return Response(serializer.data)
+    
+    @action(detail=False, methods=['get', 'patch'], permission_classes=[permissions.IsAuthenticated])
+    def preferences(self, request):
+        prefs = UserPreferences.objects.get_or_create(user=request.user)[0]
+
+        # Get
+        if request.method == 'GET':
+            return Response(UserPreferencesSerializer(prefs).data)
+        
+        # Patch
+        serializer = UserPreferencesSerializer(prefs, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
         return Response(serializer.data)
         
 class CustomObtainAuthToken(ObtainAuthToken):
