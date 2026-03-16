0087b6e6307f0221fe7e495ac8df00b5ce9c36d7
SamArndt <sarndt196@gmail.com>
2026-03-16 13:14:07 -0600

    Created ChatMessage model and seed data

--- api-server/apps/math_tutor/models.py
+++ api-server/apps/math_tutor/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+class ChatMessage(models.Model):
+  slug = models.CharField(max_length=255, unique=True)
+  llm_model = models.CharField(max_length=50)
+  user_content = models.TextField()
+  system_content = models.TextField()
+  temperature = models.FloatField(default=1.0, validators=[MinValueValidator(0.0), MaxValueValidator(2.0)])
+
+  def __str__(self):
+    return self.slug
+
 class Topic(models.Model):
   value = models.CharField(max_length=255)
 
