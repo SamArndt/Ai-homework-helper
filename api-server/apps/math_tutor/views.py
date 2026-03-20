e5a6cd43c57d2b1dd3bc3ea7cacd1cf93d4237a2
SamArndt <sarndt196@gmail.com>
2026-03-19 20:56:42 -0600

    Created `solve-problem` implementation for both generate problem and custome problem

--- api-server/apps/math_tutor/views.py
+++ api-server/apps/math_tutor/views.py
@@ -8,20 +8,30 @@ from .models import Topic, ChatMessage
 # Create your views here.
 client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
+@api_view(['POST'])
+@permission_classes([permissions.IsAuthenticated])
+def solve_math_problem(request):
+    return Response(
+        {
+            "solution": process_chat("solve-math-problem", {"topic": request.data.get('topic'), "problem": request.data.get('problem')})
+        }
+    )
+
 @api_view()
 @permission_classes([permissions.IsAuthenticated])
 def generate_math_problem(request):
     topic = Topic.objects.order_by('?').first()
-    chat_message = ChatMessage.objects.get(slug="generate-math-problem")
+    return Response({"topic": topic.value, "problem": process_chat("generate-math-problem", {"topic": topic.value})})
+
+
+def process_chat(message_slug, data_map):
+    chat_message = ChatMessage.objects.get(slug=message_slug)
 
     response = client.chat.completions.create(
         model=chat_message.llm_model,
-        temperature=chat_message.temperature,
         messages=[
             {"role": "system", "content": chat_message.system_content},
-            {"role": "user", "content": chat_message.user_content.format(topic=topic.value)},
-        ]
+            {"role": "user", "content": chat_message.user_content.format_map(data_map)},
+        ],
     )
-
-
-    return Response({"topic": topic.value, "problem": response.choices[0].message.content})
+    return response.choices[0].message.content
