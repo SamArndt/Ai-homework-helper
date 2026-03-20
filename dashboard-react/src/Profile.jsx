6d14af70546d09a9d6005c097f806389a19831d9
zpawele1 <zpawele1@asu.edu>
2026-03-20 13:06:51 -0500

    370 Profile Routing + NavLink Update

--- dashboard-react/src/Profile.jsx
+++ dashboard-react/src/Profile.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 export default function Profile() {
+    const [activityExpand, setActivityExpand] = useState(false);
     return (
 
 
@@ -54,11 +55,35 @@ export default function Profile() {
                             </div>
                     </section>
 
-                    <section className="profile-card profile-card-activity">
+                    <section className={`profile-card profile-card-activity ${activityExpand ? "profile-card-activity-expand" : ""}`}>
+
+                        <div className="profile-activity-header">
                         <h2 className="profile-card-title"> Activity</h2>
+
+
+                        <button type="button"
+                                className="profile-card-activity-dropdown"
+                                onClick={() => setActivityExpand (!activityExpand)}
+                            >
+                                {activityExpand ? "Show Less" : "Show More"}
+
+                        </button>
+                    </div>
+
+                        <div className="profile-activity-scroll">
                             <p className="profile-null">
                                 All Activity/History Will Appear Below...
                             </p>
+
+
+                            <p className="profile-card-bodytext"> Additional Dropdown Option 1</p>
+                            <p className="profile-card-bodytext"> Additional Dropdown Option 2</p>
+                            <p className="profile-card-bodytext"> Additional Dropdown Option 3</p>
+                            <p className="profile-card-bodytext"> Additional DropdownOption 4</p>
+                            <p className="profile-card-bodytext"> Additional Dropdown Option 5</p>
+                            <p className="profile-card-bodytext"> Additional Dropdown Option 6</p>
+                            <p className="profile-card-bodytext"> Additional Dropdown Option 7</p>
+                            </div>
                     </section>
 
 
