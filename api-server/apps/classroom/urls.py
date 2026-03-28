4c386b629904d64a0474a0619dabc683b688c8c9
Max Roland <maroland@asu.edu>
2026-03-28 05:23:13 -0400

    Created routes for student reports, retrieving students assigned to a teacher, generating classes (groups of students assigned to teachers for a given period of time)

--- api-server/apps/classroom/urls.py
+++ api-server/apps/classroom/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import ClassroomView
+from .views import ClassroomView, ClassesView, EnrolledView, StaffView, ReportView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter
@@ -7,4 +7,8 @@ router = DefaultRouter
 urlpatterns = [
     path('', include(router.urls)),
     path('classroom', ClassroomView.as_view(), name='classroom'),
+    path('classroom/classes', ClassesView.as_view(), name='classes'),
+    path('classroom/enrolled', EnrolledView.as_view(), name="enrolled"),
+    path('classroom/staff', StaffView.as_view(), name='staff'),
+    path('classroom/report', ReportView.as_view(), name="report"),
 ]
--- api-server/apps/classroom/views.py
+++ api-server/apps/classroom/views.py
@@ -23,3 +23,31 @@ class ClassroomView(APIView):
             getattr(request, 'data', None)
         )
         return super().http_method_not_allowed(request, *args, **kwargs)
+
+# ClassesView
+# Handles the 'classes' endpoint, which handles requests related to viewing, editing etc. classes
+# Classes are groups of students assigned to a teacher
+class ClassesView(ClassroomView):
+    http_method_names=[]
+    pass
+
+# EnrolledView
+# Handles the 'enrolled' endpoint, which handles requests relating to assigning students to a class
+class EnrolledView(ClassroomView):
+    http_method_names=[]
+    pass
+
+# StaffView
+# Handles the 'staff' endpoint, which handles requests relating to assigning staff to a class
+# Staff assigned to a class can pull student reports related to that class
+class StaffView(ClassroomView):
+    http_method_names=[]
+    pass
+
+# ReportView
+# Handles te 'report' endpoint
+# Report endpoint handles receiving generated reports for a student, requests to pull reports
+# by an assigned teacher
+class ReportView(ClassroomView):
+    http_method_names=[]
+    pass
